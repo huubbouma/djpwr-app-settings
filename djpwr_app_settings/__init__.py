a7cfee54f2663bb8b39bb93e4e8ce1e9c002de25
Huub Bouma <huub@pythonunited.com>
2025-11-27 22:00:56 +0100

    add get() and __contains__() to AppSettingDict

--- djpwr_app_settings/__init__.py
+++ djpwr_app_settings/__init__.py
@@ -1,7 +1,8 @@
 import logging
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import ProgrammingError
-from djpwr.managers import get_model, get_manager
+from djpwr.managers import get_manager
 
 default_app_config = 'djpwr.app_settings.apps.AppConfig'
 
@@ -25,16 +26,15 @@ class AppSettingDict:
             logger.error(
                 f"Catching exception in AppSettingDict.__getitem__ for {setting_label}, "
                 f" please check if migrations for this app have been run."
-
             )
             return
+        except ObjectDoesNotExist:
+            raise KeyError(setting_label)
 
         return setting.value
 
     def __setitem__(self, setting_label, value):
         app_label, model_name, setting_name = setting_label.split('.')
-
-        setting_group = get_model('.'.join([app_label, model_name]))
 
         setting = get_manager('app_settings.ApplicationSetting').get(
             group__group_name='.'.join([app_label, model_name]),
@@ -44,6 +44,25 @@ class AppSettingDict:
         setting.save()
 
         get_manager('app_settings.SettingGroup').touch_last_modified(setting.group)
+
+    def get(self, setting_label, default=None):
+        """
+        Dict-like get(): returns default when key is missing
+        """
+        try:
+            return self[setting_label]
+        except KeyError:
+            return default
+
+    def __contains__(self, setting_label):
+        """
+        Provide `in` checks: 'appname.interal.somekey' in app_settings
+        """
+        try:
+            self[setting_label]
+        except KeyError:
+            return False
+        return True
 
 
 app_settings = AppSettingDict()
